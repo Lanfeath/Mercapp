4dafd99f6be9a8281d1e83f7c6dc441c0e19242a
Bin Damba <guitcha_d@yahoo.fr>
2023-05-21 18:57:50 +0200

    changed DATABASE_URL

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ if os.environ.get('DATABASE_URL') is None:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'mercapp.db')
 else:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # need to change postgres to postgresql in DATABASE_URL
+    SQLALCHEMY_DATABASE_URI = 'postgresql://gchlevlsvlfrma:ed468281d60c9a32fe34591476aad8629da336bca86ff94fceeedc5caa662ff0@ec2-52-54-200-216.compute-1.amazonaws.com:5432/d5m3ne4dcin0ue'
