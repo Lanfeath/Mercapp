3361bf6b7d7de3bd04f75c2425c0df86f0e82441
Bin Damba <guitcha_d@yahoo.fr>
2023-05-22 17:54:55 +0200

    corrected config with script to change DB URL to fix deploy bug

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ if os.environ.get('DATABASE_URL') is None:
 else:
     # need to change postgres to postgresql in DATABASE_URL
     # Check if the URL database is already in the good format: with Postgresql instead of just postgres
-    if os.environ.get('DATABASE_URL').find('postgresql') != -1:
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres", "postgresql")
-    else:
-        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    uri = os.getenv("DATABASE_URL")  # or other relevant config var
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = uri
